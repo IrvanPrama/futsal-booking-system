ba94b39b7c66e90a433344a02d504404c208625f
IrvanPrama <bluxfusion@gmail.com>
2025-10-04 12:20:46 +0800

    tombol request member

---
+++ app/Filament/Widgets/RequestMemberWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class RequestMemberWidget extends Widget
+{
+    protected static string $view = 'filament.widgets.request-member-widget';
+}
---
+++ resources/views/filament/widgets/request-member-widget.blade.php
@@ -0,0 +1,16 @@
+<x-filament::widget>
+    <x-filament::card>
+        <div class="flex items-center justify-between">
+            <h2 class="text-lg font-bold">Request Membership</h2>
+
+            <x-filament::button
+                color="success"
+                tag="a"
+                href="https://wa.me/6285117535972?text=Halo%20Admin,%20saya%20ingin%20request%20membership."
+                target="_blank"
+            >
+                Kirim WhatsApp
+            </x-filament::button>
+        </div>
+    </x-filament::card>
+</x-filament::widget>
