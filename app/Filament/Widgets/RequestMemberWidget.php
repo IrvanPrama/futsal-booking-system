36adb0205fab62599e31a010c23c85a5fadbf162
IrvanPrama <bluxfusion@gmail.com>
2025-11-21 10:21:13 +0800

    request member hilang role 0 dan 1

--- app/Filament/Widgets/RequestMemberWidget.php
+++ app/Filament/Widgets/RequestMemberWidget.php
@@ -3,8 +3,27 @@
 namespace App\Filament\Widgets;
 
 use Filament\Widgets\Widget;
+use Illuminate\Support\Facades\Auth;
 
 class RequestMemberWidget extends Widget
 {
     protected static string $view = 'filament.widgets.request-member-widget';
+
+    public static function canView(): bool
+    {
+        $user = Auth::user();
+
+        // Jika user tidak login → sembunyikan
+        if (!$user) {
+            return false;
+        }
+
+        // Sembunyikan jika role = 0
+        if ($user->role == 2) {
+            return true;
+        }
+
+        // Selain itu tampilkan
+        return true;
+    }
 }
